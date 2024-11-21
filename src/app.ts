01b95b08266f12b8ef897aaf2d7b2302dd0f02b1
tania <tania.dimache@gmail.com>
2024-11-21 11:50:50 +0100

    calculate unique places cleaned

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,5 @@ import routerSetup from './startup/router';
 
 const app = express();
 appSetup(app);
+app.use(express.json())
 routerSetup(app);
--- src/startup/router.ts
+++ src/startup/router.ts
@@ -1,9 +1,22 @@
 import { Express, Request, Response } from 'express';
+import calculateAllCoordinates from '../service/index'
 
 const routerSetup = (app: Express) =>
   app
-  .get('/healthcheck', async (req: Request, res: Response) => {
+  .get('/healthcheck', (req: Request, res: Response) => {
     res.send('Hello World!');
-  });
+  })
+
+  .post('/tibber-developer-test/clean', (req: Request, res: Response) => {
+    const t0 = performance.now();
+    const coordinates = calculateAllCoordinates(req.body)
+    const t1 = performance.now();
+
+    res.status(201).send({
+        commands: req.body.commands.length,
+        result: coordinates.uniqueCoordinates.size,
+        duration: Number(((t1 - t0)/ 1000).toFixed(6))
+    })
+  })
 
 export default routerSetup;
