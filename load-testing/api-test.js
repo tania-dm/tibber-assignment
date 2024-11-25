befc305fbecf6d56ef7f1fed4108eac4922c06fe
tania <tania.dimache@gmail.com>
2024-11-25 13:09:49 +0100

    add monitoring and load testing

---
+++ load-testing/api-test.js
@@ -0,0 +1,36 @@
+import http from 'k6/http'
+
+export const options = {
+  stages: [
+    { duration: '5m', target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 5 minutes.
+    { duration: '10m', target: 200 }, // stay at higher 200 users for 5 minutes
+    { duration: '2m', target: 0 }, // ramp-down to 0 users
+  ],
+}
+
+const payload = JSON.stringify({
+  start: {
+    x: 11,
+    y: 22,
+  },
+  commmands: [
+    {
+      direction: 'east',
+      steps: 2,
+    },
+    {
+      direction: 'north',
+      steps: 1,
+    },
+  ],
+})
+
+const params = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+
+export default () => {
+  http.post('http://localhost:5000/tibber-developer-test/enter-path', payload, params)
+}
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import dotenvx from '@dotenvx/dotenvx'
 import express from 'express'
 import appSetup from './startup/init'
 import routerSetup from './startup/router'
+import expressStatusMonitor from 'express-status-monitor'
 
 dotenvx.config()
 const app = express()
+app.use(expressStatusMonitor())
 appSetup(app)
 app.use(express.json())
 routerSetup(app)
